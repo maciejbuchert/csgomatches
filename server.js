12cef854ab674ced35ef809ebc52c4b8cfdf510d
Maciej Buchert <maciej@buchert.pl>
2019-12-14 23:44:53 +0100

    * minor updates

--- server.js
+++ server.js
@@ -11,47 +11,51 @@ const ics = require('ics'),
         console.log('listening...');
     });
 
-let matches = [];
-
 // console.log = function(d) {
 //     logFile.write(util.format(d) + '\n');
 // };
 
-HLTV.getMatches().then((result) => {
-    for (var i = 0; i <= result.length - 1; i++) {
-        if( typeof result[i]['team1'] != 'undefined' || typeof result[i]['team2'] != 'undefined' ) {
-            if(teams.includes(result[i]['team1']['id']) || teams.includes(result[i]['team2']['id'])) {
-                if(typeof result[i]['date'] != 'undefined') {
 
-                    let match_day = moment(new Date(result[i]['date'])).locale('pl'),
-                        year = parseInt(match_day.format('YYYY')),
-                        month = parseInt(match_day.format('MM')),
-                        day = parseInt(match_day.format('DD')),
-                        hour = parseInt(match_day.format('HH')),
-                        minutes = parseInt(match_day.format('mm'));
+app.get('/', function (req, res) {
+    res.set('Content-Type', 'text/calendar; charset=utf-8');
+    res.set('Content-Disposition', 'attachment; filename=invite.ics');
 
-                    let match = {
-                        start: [year, month, day, hour, minutes],
-                        duration: {hours: Number(result[i]['format'].replace(/^BO+/i, '')), minutes: 0},
-                        title: result[i]['team1']['name'] + " vs. " + result[i]['team2']['name'],
-                        url: 'https://hltv.org',
-                    };
+    let matches = [];
 
-                    matches.push(match);
+    HLTV.getMatches().then((result) => {
+        for (i in result) {
+            if (typeof result[i]['team1'] != 'undefined' || typeof result[i]['team2'] != 'undefined') {
+                if (teams.includes(result[i]['team1']['id']) || teams.includes(result[i]['team2']['id'])) {
+                    if (typeof result[i]['date'] != 'undefined') {
+                        matches.push(getMatchObject(result[i]));
+                    }
                 }
             }
         }
-    }
 
-    let { error, value } = ics.createEvents(matches);
+        let {error, value} = ics.createEvents(matches);
 
-    if (error) {
-        console.log(error);
-    }
+        if (error) {
+            console.log(error);
+        }
 
-    app.get('/', function (req, res) {
-        res.set('Content-Type', 'text/calendar; charset=utf-8');
-        res.set('Content-Disposition', 'attachment; filename=invite.ics');
         res.send(value);
     });
 });
+
+
+function getMatchObject(data) {
+    let match_day = moment(new Date(data['date'])).locale('pl'),
+        year = parseInt(match_day.format('YYYY')),
+        month = parseInt(match_day.format('MM')),
+        day = parseInt(match_day.format('DD')),
+        hour = parseInt(match_day.format('HH')),
+        minutes = parseInt(match_day.format('mm'));
+
+    return match = {
+        start: [year, month, day, hour, minutes],
+        duration: {hours: Number(data['format'].replace(/[^0-9]/g, '')), minutes: 0},
+        title: data['team1']['name'] + " vs. " + data['team2']['name'],
+        url: 'https://hltv.org',
+    };
+}
