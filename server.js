e1fad5246ee57d91ac3a877c8121b3dbe6ca4fde
Maciej Buchert <maciej@buchert.pl>
2019-12-21 11:26:40 +0100

    * minor updates

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const ics = require('ics'),
     express = require('express'),
     // logFile = fs.createWriteStream(__dirname + '/var/log/debug.log', {flags: 'w'}),
     app = express(),
-    teams = [10549, 8813, 10494, 8068, 10426, 8248, 7681, 9797, 8209],
+    countryTeams = [],
     server = app.listen(process.env.PORT || 8080, () => {
         console.log('listening...');
     });
@@ -20,42 +20,62 @@ app.get('/', function (req, res) {
     res.set('Content-Type', 'text/calendar; charset=utf-8');
     res.set('Content-Disposition', 'attachment; filename=invite.ics');
 
-    let matches = [];
+    HLTV.getTeamRanking({country: 'Poland'}).then((teams) => {
+        for(i in teams) {
+            let team = teams[i];
+            countryTeams.push(team.team.id);
+        }
 
-    HLTV.getMatches().then((result) => {
-        for (i in result) {
-            if (typeof result[i]['team1'] != 'undefined' || typeof result[i]['team2'] != 'undefined') {
-                if (teams.includes(result[i]['team1']['id']) || teams.includes(result[i]['team2']['id'])) {
-                    if (typeof result[i]['date'] != 'undefined') {
-                        matches.push(getMatchObject(result[i]));
+        let matches = [];
+
+        HLTV.getMatches().then((result) => {
+            for (i in result) {
+                if (typeof result[i]['team1'] != 'undefined' || typeof result[i]['team2'] != 'undefined') {
+                    if (countryTeams.includes(result[i]['team1']['id']) || countryTeams.includes(result[i]['team2']['id'])) {
+                        if (typeof result[i]['date'] != 'undefined') {
+                            matches.push(getMatchObject(result[i], req.query.alarm));
+                        }
                     }
                 }
             }
-        }
 
-        let {error, value} = ics.createEvents(matches);
+            let {error, value} = ics.createEvents(matches);
 
-        if (error) {
-            console.log(error);
-        }
+            if (error) {
+                console.log(error);
+            }
 
-        res.send(value);
+            res.send(value);
+        });
     });
 });
 
 
-function getMatchObject(data) {
+function getMatchObject(data, alarm) {
     let match_day = moment(new Date(data['date'])).locale('pl'),
         year = parseInt(match_day.format('YYYY')),
         month = parseInt(match_day.format('MM')),
         day = parseInt(match_day.format('DD')),
         hour = parseInt(match_day.format('HH')),
-        minutes = parseInt(match_day.format('mm'));
+        minutes = parseInt(match_day.format('mm')),
+        alarms = [];
+
+    if('true' === alarm) {
+        alarms.push({
+            action: 'audio',
+            trigger: {hours:1,minutes:0,before:true},
+            repeat: 1,
+            attachType:'VALUE=URI',
+            attach: 'Glass'
+        });
+    }
 
     return match = {
         start: [year, month, day, hour, minutes],
         duration: {hours: Number(data['format'].replace(/[^0-9]/g, '')), minutes: 0},
         title: data['team1']['name'] + " vs. " + data['team2']['name'],
-        url: 'https://hltv.org',
+        url: "http://hltv.org/matches/" + data.id + "/match",
+        description: "Developed by Maciej Buchert (twitter.com/maciejbuchert)",
+        alarms: alarms,
     };
 }
